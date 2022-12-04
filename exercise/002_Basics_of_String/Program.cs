c6f9e2cbf6f92c3304c8ff94b513ad067d40fe9f
YoungHa Kim <ytok1108@aol.com>
2022-12-05 00:30:18 +0900

    Basics of String__result: My friend ar {firstFriend} and {sencondFriend}

---
+++ exercise/002_Basics_of_String/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+string firstFriend = "Maria";
+string sencondFriend = "Sage";
+Console.WriteLine("My friend ar {firstFriend} and {sencondFriend}");
---
+++ exercise/002_Basics_of_String/README.md
@@ -0,0 +1,3 @@
+# The Basics of Strings | C# 101 [3 of 19]
+
+https://youtu.be/JSpC7Cz64h0
