6d420e182b46de83ca0e9388f51c3c61ba5e1ac7
YoungHa Kim <ytok1108@aol.com>
2022-12-09 00:56:00 +0900

    C# Searching_String

---
+++ exercise/A003_Searching_strings/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace A003_Searching_strings
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string firstFriend = "Maria";
+      string sencondFriend = "Sage";
+      Console.WriteLine("My friend ar {firstFriend} and {sencondFriend}");
+    }
+  }
+}
+
+// result :  
+// My friend ar {firstFriend} and {sencondFriend}
