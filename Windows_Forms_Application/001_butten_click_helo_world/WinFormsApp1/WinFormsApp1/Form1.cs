4716a34a675c58cb3419c429ccb6440933f5caa0
YoungHa Kim <ytok1108@aol.com>
2022-12-08 20:00:05 +0900

    Hello Windows Forms

---
+++ Windows_Forms_Application/001_butten_click_helo_world/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,25 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello! WIndows Forms Application!";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
