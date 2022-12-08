45572eafabb50464377a7efc4defd9ba5caa3376
YoungHa Kim <ytok1108@aol.com>
2022-12-09 01:09:31 +0900

    C# tutorial

---
+++ exercise/A004_Searching/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace A004_Searching
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string sayHello = "Hello World";
+      Console.WriteLine(sayHello);
+
+      sayHello = sayHello.Replace("Hello", "Kendra");
+      Console.WriteLine(sayHello);
+
+      Console.WriteLine(sayHello.ToUpper());
+      Console.WriteLine(sayHello.ToLower());
+
+      Console.WriteLine(sayHello);
+    }
+  }
+}
+
+
+    
+// $ dotnet run
+//
+// Hello World
+// Kendra World
+// KENDRA WORLD
+// kendra world
+// Kendra World    
+
