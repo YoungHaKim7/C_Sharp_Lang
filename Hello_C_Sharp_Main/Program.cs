5e7c37160f3362d39ae524d2b7e14ae9070a6669
unknown <ytok1108@aol.com>
2022-12-09 11:44:04 +0900

    C# tutorials

---
+++ Hello_C_Sharp_Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_C_Sharp_Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! C# tutorial");
+        }
+    }
+}
