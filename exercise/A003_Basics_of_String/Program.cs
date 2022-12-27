f861d43c1f7830af04122074555018ae04e49b4b
YounghaKim7 <ytok1108@aol.com>
2022-12-27 20:49:49 +0900

    The Basics of String 3 of 19

---
+++ exercise/A003_Basics_of_String/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace A003_Basic_of_String 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstFriend = "Scott";
+            string secondFriend = "Kendra";
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+            Console.WriteLine($"The name{firstFriend} has {firstFriend.Length} letters. ");
+            Console.WriteLine($"The name{secondFriend} has {secondFriend.Length} letters. ");
+        }
+    }
+}
+
---
+++ exercise/A003_Basics_of_String/README.md
@@ -0,0 +1,17 @@
+# Result:
+
+```
+My friends are Scott and Kendra
+The nameScott has 5 letters.
+The nameKendra has 6 letters.
+```
+
+<br>
+
+# The Basics of Strings | C# 101 [3 of 19] Source
+
+https://youtu.be/JSpC7Cz64h0
+
+<br>
+
+
